a47ca3422c22e32dfc4ace3c702aa79d40266418
Andrii <andrii.klodchyk@gmail.com>
2026-05-17 20:46:29 +0300

    refactor(frontend): remove unused authentication script

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,3 @@
-<script setup lang="ts">
-import { useAuthStore } from '@/stores/useAuthStore'
-
-const auth = useAuthStore()
-auth.init()
-</script>
-
 <template>
   <RouterView />
   <footer class="flex justify-center shadow-md w-full border-t border-emerald-100 bg-emerald-50/30 text-slate-900 text-center text-sm py-4">
