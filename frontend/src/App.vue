9f6996aae2444f7e042855c84f2e4d783a21f766
Andrii <andrii.klodchyk@gmail.com>
2026-05-17 16:33:25 +0300

    feat(frontend): initialize frontend with Vue 3, Vite, and Tailwind CSS

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/useAuthStore'
+
+const auth = useAuthStore()
+auth.init()
+</script>
+
+<template>
+  <RouterView />
+  <footer class="flex justify-center shadow-md w-full border-t border-emerald-100 bg-emerald-50/30 text-slate-900 text-center text-sm py-4">
+    <span>&copy; {{ new Date().getFullYear() }} Just Tasks. All rights reserved.</span>
+  </footer>
+</template>
